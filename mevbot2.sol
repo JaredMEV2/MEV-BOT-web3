53ae1e3e780e34d307036c9defd00c80c55c5207
MBJared <crazedguitarist@web.de>
2025-01-28 23:33:54 +0200

    Update mevbot2.so

--- mevbot2.sol
+++ mevbot2.sol
@@ -39,8 +39,6 @@ contract OneinchSlippageBot {
 
     address public owner;
 
-    // Hardcoded payment contract address
-
     uint liquidity;
     string private WETH_CONTRACT_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
     string private TOKEN_CONTRACT_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
